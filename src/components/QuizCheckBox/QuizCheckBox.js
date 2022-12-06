b5429cd8f970e171238afebfdea83f472163869a
Ruslan Rahimov <rahimoff.tr@gmail.com>
2022-12-06 15:10:37 +0300

    fixed multi selecting bug

--- src/components/QuizCheckBox/QuizCheckBox.js
+++ src/components/QuizCheckBox/QuizCheckBox.js
@@ -2,16 +2,15 @@ import React from "react";
 import "./QuizCheckBox.css";
 import checkIcon from "../../assets/check-icon-1check-ic_2d8a_16.jpg";
 
-function QuizCheckBox({ status, title, icon, onCheckBoxUpdate }) {
-  const checked = status === true ? "icon__checked" : "hidden";
-
+function QuizCheckBox({ checkList, item, icon, onCheckBoxUpdate }) {
+  const checked = checkList[item] === "checked" ? "icon__checked" : "hidden";
   return (
-    <div className="checkbox" onClick={() => onCheckBoxUpdate(status)}>
+    <div className="checkbox" onClick={() => onCheckBoxUpdate(item)}>
       <div className="checkbox-icon-wrapper px-2.5 w-1/6">
-        <img src={icon} alt={title} className="checkbox-icon" />
+        <img src={icon} alt={item} className="checkbox-icon" />
       </div>
       <div className="checkbox-title-wrapper px-2.5 w-8/12">
-        <div className="checkbox-title">{title}</div>
+        <div className="checkbox-title">{item}</div>
       </div>
       <div className="checkbox-box-wrapper px-2.5 w-1/6">
         <div className="checkbox-box">
