eb88efbc63fea188817991e8c047e30d4b16df6f
Ruslan Rahimov <rahimoff.tr@gmail.com>
2022-12-07 15:12:38 +0300

    add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,10 @@
+//App.js
+export const firstCardImage = require('../assets/body1_15a3_9.png');
+export const secondCardImage = require('../assets/body2_5160_10.png');
+export const thirdCardImage = require('../assets/body3_fa53_11.png');
+export const fourthCardImage = require('../assets/body4_b846_12.png');
+export const steps = 11;
+
+//GenderButton.js
+export const maleIcon = require("../assets/male_9090_13.png");
+export const femaleIcon = require("../assets/female_c847_14.png");
